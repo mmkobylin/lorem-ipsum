644b5be4d557e51ac17582a372f1124351bd77aa
mmkobylin <mmkobylin@gmail.com>
2021-06-02 20:05:30 +0100

    basic form with ooks started

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+// import data from './data';
 
 function App() {
+
+  // paragraph amount set up as 0.
+  const [ count, setCount ] = useState(0); 
+  // empty array.
+  const [state, setstate] = useState([]);
+
+  let handleSubmit = (e) => {
+    // stops the reloading of the webpage
+    e.preventDefault();
+  }
+
+  let handleChange = (e) => {
+    e.preventDefault()
+    setCount(count = count++ )
+    
+  }
+ 
+  
   return (
-    <div className="App">
+    <div className="section-center">
+      <h3>Hipster food lorem ipsum.</h3>
+
+      <p>{count}</p>
+      <form
+        onSubmit = { handleSubmit }
+      >
+        <input name="amount" type="number"></input>
+        <button onClick = { handleChange }>Submit</button>
+      </form>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Lorem ipsum dolor sit amet, latte chilli elit, pakora do eiusmod tempor sorbet chai labore et dolore magna samosas. Chilli wrap ad falafel veniam, tapas nostrud exercitation ullamco wrap hallumi ut aliquip ex ea wrap chilli. Latte aute irure dolor cheakpeas reprehenderit in chilli velit cheesecake sorbet latte eu cheesecake falafel pariatur. Falafel chai occaecat cupidatat wrap couscous, hummus in hallumi cheesecake officia deserunt latte falafel pakora est laborum.
         </p>
