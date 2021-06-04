8ea5829df36a8310240d8a65bbae8f25a1ea24ad
mmkobylin <mmkobylin@gmail.com>
2021-06-04 21:10:35 +0100

    data file created amd added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import React, { useState } from 'react';
-// import data from './data';
+import data from './data';
 
 function App() {
 
   // paragraph amount set up as 0.
   const [ count, setCount ] = useState(0); 
   // empty array.
-  const [state, setstate] = useState([]);
+  const [text, setText] = useState([]);
 
   let handleSubmit = (e) => {
     // stops the reloading of the webpage
@@ -17,8 +17,9 @@ function App() {
 
   const handleChange = (e) => {
     e.preventDefault()
-    console.log('hello')
-   }
+    setText(data);
+
+  }
  
   
   return (
@@ -42,9 +43,9 @@ function App() {
       <header className="App-header">
 
       <article className="lorem-text">
-        <p>
-          Lorem ipsum dolor sit amet, latte chilli elit, pakora do eiusmod tempor sorbet chai labore et dolore magna samosas. Chilli wrap ad falafel veniam, tapas nostrud exercitation ullamco wrap hallumi ut aliquip ex ea wrap chilli. Latte aute irure dolor cheakpeas reprehenderit in chilli velit cheesecake sorbet latte eu cheesecake falafel pariatur. Falafel chai occaecat cupidatat wrap couscous, hummus in hallumi cheesecake officia deserunt latte falafel pakora est laborum.
-        </p>
+        {/* {Text.map (( item, index) => {
+          return
+        })} */}
       </article>
 
       </header>
