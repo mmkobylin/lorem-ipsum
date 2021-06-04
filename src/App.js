df62d1cda277be69d8188328a7d74c2551dd7319
mmkobylin <mmkobylin@gmail.com>
2021-06-04 20:53:30 +0100

    handleSubmit test worked

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
   let handleSubmit = (e) => {
     // stops the reloading of the webpage
     e.preventDefault();
+    console.log('hello')
   }
 
   const handleChange = (e) => {
@@ -39,9 +40,12 @@ function App() {
         <button type ="submit" className="btn">generate</button>
       </form>
       <header className="App-header">
+
+      <article className="lorem-text">
         <p>
           Lorem ipsum dolor sit amet, latte chilli elit, pakora do eiusmod tempor sorbet chai labore et dolore magna samosas. Chilli wrap ad falafel veniam, tapas nostrud exercitation ullamco wrap hallumi ut aliquip ex ea wrap chilli. Latte aute irure dolor cheakpeas reprehenderit in chilli velit cheesecake sorbet latte eu cheesecake falafel pariatur. Falafel chai occaecat cupidatat wrap couscous, hummus in hallumi cheesecake officia deserunt latte falafel pakora est laborum.
         </p>
+      </article>
 
       </header>
     </div>
