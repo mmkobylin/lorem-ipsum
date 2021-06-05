ddd6dc0edc6c9ef60c9d7aeb66950cf1b100bbae
mmkobylin <mmkobylin@gmail.com>
2021-06-05 11:43:16 +0100

    data now displays using map function

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
 import './App.css';
 import React, { useState } from 'react';
+// passing data from another 
 import data from './data';
 
 function App() {
 
   // paragraph amount set up as 0.
   const [ count, setCount ] = useState(0); 
-  // empty array.
+  // empty OBJECT for some fucking reason
   const [text, setText] = useState([]);
 
   let handleSubmit = (e) => {
     // stops the reloading of the webpage
     e.preventDefault();
-    console.log('hello')
-  }
-
-  const handleChange = (e) => {
-    e.preventDefault()
+    // sets up text
+    // cries that data is an object"
+    console.log(typeof(text));
     setText(data);
-
   }
  
-  
   return (
     <div className="section-center">
       <h3>Hipster food lorem ipsum.</h3>
@@ -35,20 +32,20 @@ function App() {
         name="amount" id="amount" type="number" min = "0" max = "10"
         // input controlled - by setCount changing count to e.target.value
         //e sets event 
-        onChange = { (e) => setCount(e.target.value ) }
+        onChange = { (e) => setCount( e.target.value ) }
         
         ></input>
+        {/* submit button */}
         <button type ="submit" className="btn">generate</button>
       </form>
-      <header className="App-header">
 
       <article className="lorem-text">
-        {/* {Text.map (( item, index) => {
-          return
-        })} */}
+        { text.map ( (item, index) => {
+          return <p key = { index } > { item } </p>
+
+        })}
       </article>
 
-      </header>
     </div>
   );
 }
