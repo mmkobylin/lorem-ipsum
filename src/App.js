2dc42c161afec85b4f2120bfb21708f91ebe52ce
mmkobylin <mmkobylin@gmail.com>
2021-06-06 20:52:30 +0100

    each aary properly displaying on console

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
   let handleSubmit = (e) => {
     // stops the reloading of the webpage
     e.preventDefault();
-
+    
     // this part defines the amount of paragraphs.
     if (data.length < count ) { 
       // how many times we need the data???
@@ -25,18 +25,89 @@ function App() {
 
       setText(dataPlus.slice(0, count));
 
+      const words = text.trim().split(" ");
+
+      console.log(words);
+
+
     } else {
+
       setText(data.slice( 0, count));
+
+     for (var i = 0; i < data.length; i++) {
+      // displays each array sepearately; good!
+        const words = data[i].trim().split(" ");
+
+        for (var j = 3; j < words.length; j++) {
+        
+          function randomise() {
+              // checking each item in words array except last one,
+              var lastChar = words[j][words[j].length - 1];
+  
+              var foodRandom = ['chilli', 'cheesecake', 'chai', 'latte', 'sorbet',  'wrap', 'falafel', 'hallumi', 'tapas', 'samosas', 'couscous', 'pakora', 'spicy', 'hummus', 'cheakpeas'];
+  
+              // randomising variable
+              var random = foodRandom[Math.floor(Math.random()*foodRandom.length)];
+  
+              // sorting punctuation
+              if (lastChar === ',' ) {
+                  // items in array randomiezed and floored
+                  words[j] = random + ',' ;
+              }
+              else if  (lastChar === '.' ) {
+                  words[j] = random + '.';
+              }
+  
+              else {
+                  words[j] = random;
+              }
+          }
+      
+          // sorting capitals
+          function capitals() {
+              if (words[j - 1][words[j-1].length - 1] === '.' ) {
+                  // capitalize first letter //slice first letter of the word and add rest
+                  words[j] = words[j].charAt(0).toUpperCase() + words[j].slice(1);
+              }
+          }
+          
+          function duplication() {  
+         
+              // using slice as it has to ignore dots and capitals.
+              if (words[j].slice(1,3) === words[j - 1].slice(1,3) ) {
+  
+                  randomise();
+  
+              }
+          }        
+  
+          // check for each word;
+          if (Math.random() < 0.5 ) {
+  
+              randomise();
+  
+          }
+  
+          // check for duplicates, then changes to capitals;
+          duplication()
+  
+          capitals();
+  
+        }
+
+        // displays each array sepearately; good!
+        console.log(words)
+     }
+
 
     }
   }
 
-  let handleClick = () => {
+  let handleClick = (array) => {
     // getting the data, iterating through each object.
-    for (var i = 0; i < data.length; i++) {
 
       //splitting each item data[i] into an array
-      const words = data[i].trim().split(" ");
+      const words = array.trim().split(" ");
 
       // add randomisation
       for (var j = 3; j < words.length; j++) {
@@ -51,11 +122,11 @@ function App() {
             var random = foodRandom[Math.floor(Math.random()*foodRandom.length)];
 
             // sorting punctuation
-            if (lastChar == ',' ) {
+            if (lastChar === ',' ) {
                 // items in array randomiezed and floored
                 words[j] = random + ',' ;
             }
-            else if  (lastChar == '.' ) {
+            else if  (lastChar === '.' ) {
                 words[j] = random + '.';
             }
 
@@ -66,7 +137,7 @@ function App() {
     
         // sorting capitals
         function capitals() {
-            if (words[j - 1][words[j-1].length - 1] == '.' ) {
+            if (words[j - 1][words[j-1].length - 1] === '.' ) {
                 // capitalize first letter //slice first letter of the word and add rest
                 words[j] = words[j].charAt(0).toUpperCase() + words[j].slice(1);
             }
@@ -75,7 +146,7 @@ function App() {
         function duplication() {  
        
             // using slice as it has to ignore dots and capitals.
-            if (words[j].slice(1,3) == words[j - 1].slice(1,3) ) {
+            if (words[j].slice(1,3) === words[j - 1].slice(1,3) ) {
 
                 randomise();
 
@@ -96,15 +167,13 @@ function App() {
 
       }
       // displays as words ( sentences )
-      setWords(words.join(' ')); 
-      }
+      setWords(array.join(' ')); 
   }
  
   return (
     <>      
       <h3>Hipster food lorem ipsum.</h3>
 
-      {/* displays data */}
 
       <p>{words}</p>
 
