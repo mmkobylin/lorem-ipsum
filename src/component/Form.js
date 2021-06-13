9cd382839f3e2aae9a476d2bd96216b6d1db0976
mmkobylin <mmkobylin@gmail.com>
2021-06-13 18:01:55 +0100

    text now appearing correctly in component, but still rerendering when the paragraph changes

--- src/component/Form.js
+++ src/component/Form.js
@@ -55,11 +55,61 @@ const Form = () => {
         </form>
       </section>
       <article className="lorem-text">
-        { text.map( ( index, value ) => {
-          <p key = { index }>{ value } </p>
-        } ) 
+    { text.map ( ( item, index ) => {
+
+      var words = item.trim().split(" ");
+
+      for (var i = 3; i < words.length; i++) {
+
+      function randomise() {
+        // checking each item in words array except last one,
+        var lastChar = words[i][words[i].length - 1];
+
+        var foodRandom = ['chilli', 'cheesecake', 'chai', 'latte', 'sorbet',  'wrap', 'falafel', 'hallumi', 'tapas', 'samosas', 'couscous', 'pakora', 'spicy', 'hummus', 'cheakpeas'];
+
+        // randomising variable
+        var random = foodRandom[Math.floor(Math.random()*foodRandom.length)];
+
+        // sorting punctuation
+        if (lastChar == ',' ) {
+          // items in array randomiezed and floored
+          words[i] = random + ',' ;
         }
-      </article>
+        else if  (lastChar == '.' ) {
+          words[i] = random + '.';
+        }
+        else {
+          words[i] = random;
+        }
+      }
+
+      // sorting capitals
+      function capitals() {
+        if (words[i - 1][words[i-1].length - 1] == '.' ) {
+          // capitalize first letter //slice first letter of the word and add rest
+          words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+        }
+      }
+  
+      function duplication() {
+      // using slice as it has to ignore dots and capitals.
+        if (words[i].slice(1,3) == words[i - 1].slice(1,3) ) {
+          randomise();
+        }
+      }        
+
+      // check for each word;
+      if (Math.random() < 0.5 ) {
+        randomise();
+      }
+
+    // check for duplicates, then changes to capitals;
+    duplication()
+    capitals()
+  }     
+return <p key = { index } > { words.join( ' ' )} </p>
+})}
+    </article>
       </>
     )
 }
